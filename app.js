7177e32eec73d75374e965d3029d85d6925a5bd8
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-03-31 16:15:56 -0700

    add logic handling two expressions

--- app.js
+++ app.js
@@ -1,67 +1,44 @@
 var Calculator = function() {
-  this.previousValue = 0;
-  this.currentValue = 0;
-  this.result = 0;
-  this.values = [];
-  this.operations = [];
+  this.value = 0;
+  this.values = [0, 0];
+  this.operation = null;
+  // this.operations = [];
+
 
   this.add = this.add.bind(this);
   // this.subtract = this.subtract.bind(this);
   // this.multiply = this.multiply.bind(this);
   // this.divide = this.divide.bind(this);
   this.equals = this.equals.bind(this);
-  this.execute = this.execute.bind(this);
-  this.update = this.update.bind(this);
-
-
-
+  // this.execute = this.execute.bind(this);
+  // this.update = this.update.bind(this);
 };
 
-Calculator.prototype.execute = function() {
-  console.log('execute called');
-  this.operations.shift()();
-  this.values = [this.result];
+//initial val is 0
+
+//on add click, you're adding to initial val
+
+Calculator.prototype.add = function(val) {
+  this.value += val;
+  this.operation = this.add;
+  return this.value;
 };
 
-Calculator.prototype.update = function() {
-  if (this.operations.length === 2) {
-    console.log('operations.length === 2');
-    this.execute();
-    console.log(this.result);
-  }
-
-  console.log(this.operations[0] === this.equals);
-  console.log(this.operations[0]);
-  if (this.operations[0] === this.equals) {
-    this.currentValue = this.result;
-    this.values = [];
-    this.operations = [];
-  }
-};
-
-Calculator.prototype.add = function() {
-  this.result = this.values[0] + this.values[1];
-  return this.result;
-};
-
-Calculator.prototype.equals = function() {
-  this.values = [this.result];
-  this.operations = [];
-  return this.result;
+Calculator.prototype.equals = function(val) {
+  return this.operation.call(this, val);
+  // return this.value;
 };
 
 var calc = new Calculator();
-var add = calc.add;
-var equals = calc.equals;
-
-
 
 var newCalculator = function() {
   calc = new Calculator();
   console.log('calc', calc);
-  $('.display').text(calc.result);
+  $('.display').text(calc.value);
   return calc;
 };
+
+var inputValue = '';
 
 
 $('.key').click(function() {
@@ -71,13 +48,12 @@ $('.key').click(function() {
     val = '*';
   }
 
-  if (typeof val !== 'number') {
-    calc.values.push(calc.currentValue);
-  }
+
 
   if (val === '+' || val === '-' || val === '*' || val === '/' || val === '=') {
+    inputValue = parseInt(inputValue);
     if (val === '+') {
-      calc.operations.push(add);
+      calc.add(inputValue);
     }
     if (val === '-') {
       calc.operation = calc.subtract;
@@ -89,12 +65,18 @@ $('.key').click(function() {
       calc.operation = calc.divide;
     }
     if (val === '=') {
-      calc.operations.push(equals);
+      calc.equals(inputValue);
     }
+    console.log(calc.value);
+    $('.display').text(calc.value);
 
-    // calc.previousValue = calc.currentValue;
-    calc.currentValue = 0;
+    inputValue = '';
+  } else {
+
+    inputValue += val;
+    $('.display').text(inputValue);
   }
+
 
 
   // if (val === '=') {
@@ -107,8 +89,8 @@ $('.key').click(function() {
   // }
 
   // console.log(val);
-  calc.currentValue += val;
-  $('.display').text(calc.currentValue);
+  // calc.currentValue += val;
+
 });
 
 // $('.display').text(calc.currentValue);
